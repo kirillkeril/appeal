7a7b0646bd297c6122abc5f01587ef5f3722fb1d
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-11-23 20:16:16 +0300

    fix model

--- back/src/appeal/dto/create-appeal.dto.ts
+++ back/src/appeal/dto/create-appeal.dto.ts
@@ -2,11 +2,5 @@ import {ApiProperty} from "@nestjs/swagger";
 
 export class CreateAppealDto {
     @ApiProperty()
-    author: string;
-
-    @ApiProperty()
-    title: string;
-
-    @ApiProperty()
     body: string;
 }
--- back/src/appeal/entities/appeal.entity.ts
+++ back/src/appeal/entities/appeal.entity.ts
@@ -5,17 +5,8 @@ export type AppealDocument = HydratedDocument<Appeal>;
 
 @Schema()
 export class Appeal {
-    @Prop()
-    author: string;
-
-    @Prop()
-    title: string;
-
     @Prop({required: true})
     body: string;
-
-    @Prop({required: false})
-    tags: [string];
 }
 
 export const AppealSchema = SchemaFactory.createForClass(Appeal);
